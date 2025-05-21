280390257df123f69cfe06857c160282dffdb8ab
SebasGirald1003 <sebasgiraldo1003@gmail.com>
2025-05-21 15:30:50 -0500

    regla falsa implementado

--- cap_uno/urls.py
+++ cap_uno/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('biseccion/', views.biseccion_view, name='biseccion'),
+    path('regla-falsa/', views.regla_falsa_view, name='regla_falsa'),
 ]
--- cap_uno/views.py
+++ cap_uno/views.py
@@ -20,7 +20,6 @@ def biseccion_view(request):
             tol      = form.cleaned_data['tolerancia']
             max_iter = form.cleaned_data['max_iter']
             try:
-                # Permitir funciones matemáticas seguras
                 allowed_names = {k: getattr(math, k) for k in dir(math) if not k.startswith("__")}
                 f = lambda x: eval(f_str, {"__builtins__": {}}, {**allowed_names, 'x': x})
             except Exception as e:
@@ -46,6 +45,50 @@ def biseccion_view(request):
         form = BiseccionForm()
     
     return render(request, 'biseccion.html', {
+        'form': form,
+        'tabla': tabla,
+        'raiz': raiz
+    })
+
+def regla_falsa_view(request):
+    tabla = None
+    raiz = None
+    if request.method == 'POST':
+        form = BiseccionForm(request.POST)
+        if form.is_valid():
+            f_str    = form.cleaned_data['funcion']
+            a        = form.cleaned_data['a']
+            b        = form.cleaned_data['b']
+            tol      = form.cleaned_data['tolerancia']
+            max_iter = form.cleaned_data['max_iter']
+            try:
+                allowed_names = {k: getattr(math, k) for k in dir(math) if not k.startswith("__")}
+                f = lambda x: eval(f_str, {"__builtins__": {}}, {**allowed_names, 'x': x})
+            except Exception as e:
+                messages.error(request, f"Error en la función: {e}")
+            else:
+                if f(a) * f(b) > 0:
+                    messages.error(request, "f(a) y f(b) deben tener signos opuestos.")
+                else:
+                    tabla = []
+                    for i in range(1, max_iter + 1):
+                        fa = f(a)
+                        fb = f(b)
+                        xm = b - (fb * (a - b)) / (fa - fb)  # Regla falsa
+                        fxm = f(xm)
+                        error = abs(fxm)
+                        tabla.append((i, a, xm, b, fxm, error))  
+                        if isclose(fxm, 0, abs_tol=tol) or abs(b - a) < tol:
+                            raiz = xm
+                            break
+                        if fa * fxm < 0:
+                            b = xm
+                        else:
+                            a = xm
+    else:
+        form = BiseccionForm()
+    
+    return render(request, 'regla_falsa.html', {
         'form': form,
         'tabla': tabla,
         'raiz': raiz
